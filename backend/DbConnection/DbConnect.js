e2831757bc183283c44681e83279ab29b7fee795
Manya2418 <manyasahu94@gmail.com>
2024-07-09 10:31:31 +0530

    update

--- backend/DbConnection/DbConnect.js
+++ backend/DbConnection/DbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const dbconnection=mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep32d.mongodb.net/')
+const dbconnection=mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep32d.mongodb.net/StackOver')
 .then(()=>{
     console.log("Connected")
 }).catch("not connected")
