39e07cc13970d3936cf9dd8f6d53f7e8414a29c9
Manya2418 <manyasahu94@gmail.com>
2024-07-30 19:14:45 +0530

    update

--- backend/DbConnection/DbConnect.js
+++ backend/DbConnection/DbConnect.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const dbconnection=async()=>{
     try{
         await mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep32d.mongodb.net/StackOver',{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             poolSize: 10
         })
         console.log("Connected")
