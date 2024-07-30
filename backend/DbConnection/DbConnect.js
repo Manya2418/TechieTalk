af929645b2fc7d192f03143e6bc1729d579b5446
Manya2418 <manyasahu94@gmail.com>
2024-07-30 21:07:03 +0530

    update

--- backend/DbConnection/DbConnect.js
+++ backend/DbConnection/DbConnect.js
@@ -1,14 +1,28 @@
+// import mongoose from "mongoose";
+
+// const dbconnection=async()=>{
+//     try{
+//         await mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep32d.mongodb.net/StackOver')
+//         console.log("Connected")
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+   
+// }
+
 import mongoose from "mongoose";
 
-const dbconnection=async()=>{
-    try{
-        await mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep32d.mongodb.net/StackOver')
-        console.log("Connected")
-    }
-    catch(err){
-        console.log(err);
-    }
-   
-}
+const dbconnection = () => {
+  mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep32d.mongodb.net/StackOver')
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
+};
+
 export default dbconnection;
+
 
