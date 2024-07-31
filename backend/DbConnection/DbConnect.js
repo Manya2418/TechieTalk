052fb0f60619f2c5997a29a7d14001e05d5cd628
Manya2418 <manyasahu94@gmail.com>
2024-07-31 10:28:00 +0530

    update

--- backend/DbConnection/DbConnect.js
+++ backend/DbConnection/DbConnect.js
@@ -7,5 +7,14 @@ const dbconnection=mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep3
     console.log("Connected")
 }).catch("not connected")
 
+// const dbconnection=async()=>{
+//     try{
+//         mongoose.connect('mongodb+srv://manya:manya123@cluster0.ixep32d.mongodb.net/StackOver')
+//         console.log("connected")
+//     }catch(err){
+//         console.log("not connected")
+//     }
+// }
+
 export default dbconnection;
 
